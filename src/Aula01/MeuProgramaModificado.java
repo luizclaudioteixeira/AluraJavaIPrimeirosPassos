ef816c1e92a1b2c6af32f072949200dc97a58e06
tester <testandocommit@gmail.com>
2017-11-03 21:48:43 -0200

    Exercicio 3 - mais uma linha de codigo

--- src/Aula01/MeuProgramaModificado.java
+++ src/Aula01/MeuProgramaModificado.java
@@ -4,6 +4,8 @@ public class MeuProgramaModificado {
 	
 	public static void main(String[] args) {
 		System.out.println("Primeiros passos com java - Aula 1");
+		
+		System.out.println("Exercício 3 - adicionando mais uma linha de código");
 	}
 
 }
