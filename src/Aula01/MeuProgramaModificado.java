846a2cd2e2d7179f621fdb257ce4a5b93a9713f3
tester <testandocommit@gmail.com>
2017-11-03 21:56:40 -0200

    Exercicio 3 - imprimindo 2 linhas com \n

--- src/Aula01/MeuProgramaModificado.java
+++ src/Aula01/MeuProgramaModificado.java
@@ -3,9 +3,9 @@ package Aula01;
 public class MeuProgramaModificado {
 	
 	public static void main(String[] args) {
-		System.out.println("Primeiros passos com java - Aula 1");
+		System.out.println("Primeiros passos com java - Aula 1\nSegunda Linha usando apenas 1 syso");
 		
-		System.out.println("Exercício 3 - adicionando mais uma linha de código");
+	
 	}
 
 }
