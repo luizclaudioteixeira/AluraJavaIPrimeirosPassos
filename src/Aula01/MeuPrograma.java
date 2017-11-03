9be37c96fc1e2030438613ca7df2765973e3e6b7
tester <testandocommit@gmail.com>
2017-11-03 21:28:02 -0200

    --all

---
+++ src/Aula01/MeuPrograma.java
@@ -0,0 +1,10 @@
+package Aula01;
+
+	class MeuPrograma {
+		public static void main (String args[]) { // Método Main
+		
+		System.out.println("Minha primeira aplicação Java!"); // imprime na tela
+		
+	}
+
+}
---
+++ src/Aula01/MeuProgramaModificado.java
@@ -0,0 +1,9 @@
+package Aula01;
+
+public class MeuProgramaModificado {
+	
+	public static void main(String[] args) {
+		System.out.println("Primeiros passos com java - Aula 1");
+	}
+
+}
